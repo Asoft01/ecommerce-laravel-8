323d2f8965cfb45a1ee244bc88898e2caed57121
Adeleke Hammed Lekan <lekhad19@gmail.com>
2023-07-07 09:46:43 +0100

    Added Category

---
+++ app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_name', 
+        'description', 
+        'is_active', 
+        'image'
+    ]; 
+
+    public static function image($filename, $category){
+        if($request->hasFile($filename)){
+            $file = $request->file($filename); 
+            $extension = $file->getClientOriginalExtension(); 
+            $filename = time().'.'.$extension; 
+            $file->move('images/categories/', $filename); 
+            $category->image = $filename; 
+        }
+    }
+}
